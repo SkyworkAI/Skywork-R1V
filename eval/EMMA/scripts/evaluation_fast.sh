5315dc5f190a4335c1ff6aef9d35a4607df34b7f
彭健(Jian Peng) <145831599+Catpp01@users.noreply.github.com>
2025-07-08 18:14:23 +0800

    Revert "Add evaluation files"

--- eval/EMMA/scripts/evaluation_fast.sh
+++
@@ -1,4 +0,0 @@
-python evaluate.py \
---results_dir 'path_to_your_results_dir' \
---response_label 'response' \
---save_every 20
--- eval/MMK12/launch_skywork_r1v3.sh
+++
@@ -1,4 +0,0 @@
-set -x
-MODEL_PATH=/path/to/r1v3-model
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m vllm.entrypoints.openai.api_server --model $MODEL_PATH --port 8000 --trust-remote-code --max_model_len 32768 --served-model-name r1v3-alpha --limit-mm-per-prompt "image=60" --gpu_memory_utilization 0.8 --tensor-parallel-size 8 --dtype auto 
-
